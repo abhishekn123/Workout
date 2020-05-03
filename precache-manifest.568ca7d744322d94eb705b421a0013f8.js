d6d1f12182d6a0e5c688ca37b5019b7ddaba7bce
abhishekn123 <abhiabhishekn164@gmail.com>
2020-05-03 14:18:40 +0530

    Updates

---
+++ precache-manifest.568ca7d744322d94eb705b421a0013f8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "903cfe5aada0762523ed",
+    "url": "/Workout/static/js/app.8d06b485.chunk.js"
+  },
+  {
+    "revision": "7f67b7a9ff89417a74db",
+    "url": "/Workout/static/js/runtime~app.dbfbb58f.js"
+  },
+  {
+    "revision": "e7cdc6aae8fb3e973cdc",
+    "url": "/Workout/static/js/2.3c9e3d63.chunk.js"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/Workout/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/Workout/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/Workout/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "6dd34384b341959a45abae08c9fdbc91",
+    "url": "/Workout/static/media/1.6dd34384.png"
+  },
+  {
+    "revision": "a044acfbd98f3c4ef1e8bdf77a3b27da",
+    "url": "/Workout/static/media/3.a044acfb.png"
+  },
+  {
+    "revision": "81f400b804bbe788a734a29c129b6285",
+    "url": "/Workout/static/media/5.81f400b8.png"
+  },
+  {
+    "revision": "48e86c7ff819c41e8487cf7b9a810eeb",
+    "url": "/Workout/static/media/9.48e86c7f.png"
+  },
+  {
+    "revision": "66f6d181b4153fc248465c11a9a85c0d",
+    "url": "/Workout/static/media/4.66f6d181.png"
+  },
+  {
+    "revision": "fba453b57d0adc820d6f4b9d9e5854c9",
+    "url": "/Workout/static/media/8.fba453b5.png"
+  },
+  {
+    "revision": "48b04a99c1e9159600307606a3756127",
+    "url": "/Workout/static/media/7.48b04a99.png"
+  },
+  {
+    "revision": "a2bb97edc6179f81eb0a2afeccdcc562",
+    "url": "/Workout/static/media/Song 14 - Rock.a2bb97ed.mp3"
+  },
+  {
+    "revision": "4d743ca53d31e080e49d04fc3cbf0a82",
+    "url": "/Workout/static/media/Song 18 - Rock.4d743ca5.mp3"
+  },
+  {
+    "revision": "78d7e89566ad99662f7255940c5f3ccb",
+    "url": "/Workout/static/media/Song 34 - Pop.78d7e895.mp3"
+  },
+  {
+    "revision": "677a1135ea54aed7cafea899a5e1670b",
+    "url": "/Workout/static/media/LCOT.677a1135.png"
+  },
+  {
+    "revision": "7624a44e87dd0330a3adb45d82998471",
+    "url": "/Workout/static/media/profile.7624a44e.jpg"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/Workout/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/Workout/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/Workout/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/Workout/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/Workout/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/Workout/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/Workout/favicon.ico"
+  },
+  {
+    "revision": "e350e9703c6a7d8fef0dcbca9d052d89",
+    "url": "/Workout/index.html"
+  },
+  {
+    "revision": "3c36f23a348abb7cfd68dac43582f7dd",
+    "url": "/Workout/manifest.json"
+  },
+  {
+    "revision": "3c59a300b5152ae8cae2512c80d3f13a",
+    "url": "/Workout/register-service-worker.js"
+  }
+];
